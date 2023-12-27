282ed70bd7a0c44be86321fc59a5722e1c8462f3
Jared F. King <101604891+Neuraljac@users.noreply.github.com>
2023-12-27 11:32:38 -0500

    Update fizzBuzz_python.py

--- Interview Preparation Kits/1 Week Prep Kit/Day 1/fizzBuzz/fizzBuzz_python.py
+++ Interview Preparation Kits/1 Week Prep Kit/Day 1/fizzBuzz/fizzBuzz_python.py
@@ -1,4 +1,6 @@
-# Define the function FizzBuzz, which takes any number n and tests whether it is divisible by three and five. If so, it prints "FizzBuzz". If it is only divisible by 3, it prints "Fizz", and if it is only divisible by 5, it prints "Buzz". If none of these options are true, it prints the number.
+# Define the function FizzBuzz, which takes any number n and tests whether it is divisible by three and five. If so, it prints "FizzBuzz". 
+#If it is only divisible by 3, it prints "Fizz", and if it is only divisible by 5, it prints "Buzz". 
+#If none of these options are true, it prints the number.
 def fizzBuzz(n):
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
